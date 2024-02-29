d22616e19123aaba4919db41076988fa5e4c29c0
DilnLemos <dilan.lemos@correounivalle.edu.co>
2024-02-29 11:28:36 -0500

    Subida archivos al git

---
+++ Clase#2/src/App.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Jeferson chupeme la verga!");
+        JOptionPane.showMessageDialog(null, "Pene", "ola", JOptionPane.CANCEL_OPTION);
+      
+    }
+}

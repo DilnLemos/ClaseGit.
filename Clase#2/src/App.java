e05a7f75bc78ac451b3c9e81f866df7e44622d46
DilnLemos <dilan.lemos@correounivalle.edu.co>
2024-02-29 22:51:19 -0500

    "Cambio de mensaje en los cuadros de texto"

--- Clase#2/src/App.java
+++ Clase#2/src/App.java
@@ -2,8 +2,8 @@ import javax.swing.JOptionPane;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Jeferson chupeme la verga!");
-        JOptionPane.showMessageDialog(null, "Pene", "ola", JOptionPane.CANCEL_OPTION);
-      
+        System.out.println("This is a Programming Test B)");
+        JOptionPane.showMessageDialog(null, "People like parkour for several reasons: the personal challenge that exist, the freedom of movement, the adrenaline and exitement, the community of the sport and the active lifestyle ", "PARKOUR", JOptionPane.CANCEL_OPTION);
+        JOptionPane.showMessageDialog(null, "Parkour was create by David Belle and his friends in 1980's and has since expanded globally becoming in a recognized discipline ", "PARKOUR", JOptionPane.CANCEL_OPTION);
     }
 }
